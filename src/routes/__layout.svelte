ac204721391d552ca1bc2a3d9abd1961041e2f99
styxpilled <styxpilled.dev@gmail.com>
2022-04-26 20:19:25 +0200

    fix: original transform flashing

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,12 @@
 <script>
   import "$styles/app.pcss";
+  import { onMount } from "svelte";
+  onMount(() => {
+    // get html tag and set opacity to 1
+    const html = document.querySelector("html");
+    html.style.opacity = "1";
+    html.style.visibility = "visible";
+  });
 </script>
 
 <slot />
