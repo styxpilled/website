76a9599f45f4ef698d87c1a9c023d080b8bfbe4c
styxpilled <styxpilled.dev@gmail.com>
2022-06-02 19:28:23 +0200

    fix: info component

---
+++ src/lib/Info.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let title: string = "Astrolabe";
+</script>
+
+<div class="info">
+  <h1>{title}</h1>
+</div>
+
+<style lang="postcss">
+  @import "variables.pcss";
+  .info {
+    background: transparent;
+    padding: 1em;
+    text-align: center;
+    position: absolute;
+  }
+</style>
