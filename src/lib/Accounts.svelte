57e09928ccdab017808d5d31d02dbc0239f13d87
styxpilled <styxpilled.dev@gmail.com>
2022-08-29 21:37:38 +0200

    feat: removed old components

--- src/lib/Accounts.svelte
+++
@@ -1,8 +0,0 @@
-<div>
-  <h3>Accounts:</h3>
-  <ul>
-    <li><a href="https://github.com/styxpilled">github</a></li>
-    <li><a href="https://twitter.com/styxpilled">twitter</a></li>
-    <li>discord</li>
-  </ul>
-</div>
